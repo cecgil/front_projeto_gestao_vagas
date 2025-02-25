dd38eae079ed0aa90b63cc9dbed8438c9ae9f0a5
Carlos <dudigil6@gmail.com>
2025-02-25 18:00:27 -0300

    Enviando dadoas do filter para findJobService

--- src/main/java/br/com/cecgil/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/cecgil/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.cecgil.front_gestao_vagas.modules.candidate.service.CandidateService;
+import br.com.cecgil.front_gestao_vagas.modules.candidate.service.FindJobService;
 import br.com.cecgil.front_gestao_vagas.modules.candidate.service.ProfileCandidateService;
 import jakarta.servlet.http.HttpSession;
 
@@ -31,6 +32,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateService profileCandidateService;
+
+    @Autowired
+    private FindJobService findJobsService;
     
     @GetMapping("/login")
     public String login(){
@@ -82,10 +86,25 @@ public class CandidateController {
 
     @GetMapping("/jobs")
     @PreAuthorize("hasRole('CANDIDATE')")
-    public String  jobs(){
+    public String  jobs(Model model, String filter){
+
+        try{
+            if(filter != null) {
+                //model.addAttribute("jobs", filter)
+                this.findJobsService.execute(getToken(), filter);
+            }
+        }catch(HttpClientErrorException e){
+            return "redirect:/candidate/login";
+        }
         return "candidate/jobs";
+
     }
-    
+
+    private String getToken(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getDetails().toString();
+    }
+
     
     
 }
--- src/main/java/br/com/cecgil/front_gestao_vagas/modules/candidate/service/FindJobService.java
+++ src/main/java/br/com/cecgil/front_gestao_vagas/modules/candidate/service/FindJobService.java
@@ -10,22 +10,23 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 import org.springframework.web.client.RestTemplate;
-
-import br.com.cecgil.front_gestao_vagas.modules.DTO.ProfileUserDTO;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
-public class ProfileCandidateService {
-    
+public class FindJobService {
 
-    public ProfileUserDTO execute(String token) {
+
+        public String execute(String token, String filter) {
         RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
 
         HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/candidate/job")
+        .queryParam("filter", filter);
         try{
-            var result = rt.exchange("http://localhost:8080/candidate/", HttpMethod.GET, request, ProfileUserDTO.class);
-            System.out.println(result);
+            var result = rt.exchange(builder.toUriString(), HttpMethod.GET, request, String.class);
             return result.getBody();
         }catch(Unauthorized ex){
             throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
